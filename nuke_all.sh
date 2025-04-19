381dd806b08acb0350efa6128c9a586965e57e9e
MTVirux <joaofraga96@outlook.com>
2025-04-19 23:10:42 +0000

    Modified script to ignore errors

--- nuke_all.sh
+++ nuke_all.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker container rm -f $(docker container ls -qa) && docker rmi -f $(docker image ls -qa) && docker builder prune -fa
+docker container rm -f $(docker container ls -qa) ; docker rmi -f $(docker image ls -qa) ; docker builder prune -fa
