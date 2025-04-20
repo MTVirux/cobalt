693d7a00105282b0dc51f949c0d5a9cb6850d199
MTVirux <joaofraga96@outlook.com>
2025-04-20 12:32:18 +0000

    Add curl nano and docker-compose to install script

--- prep_ubuntu.sh
+++ prep_ubuntu.sh
@@ -5,4 +5,5 @@ sudo apt-get update && sudo apt-get upgrade
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
 rm get-docker.sh
+sudo apt-get install -y curl nano docker-compose
 sudo apt autoremove
