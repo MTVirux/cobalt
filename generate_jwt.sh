441f22e5f518e40b85ddb618eb2d931d37a74a20
MTVirux <joaofraga96@outlook.com>
2025-04-20 12:47:37 +0000

    Added separators visual clarity and shebang

--- generate_jwt.sh
+++ generate_jwt.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
 sudo apt-get -y install npm
 npm install -g pnpm
+echo "----------------JWT Token-------------------"
 pnpm exec node -e "console.log(require('crypto').randomBytes(48).toString('base64').replace(/[^a-zA-Z0-9]/g, '').slice(0, 64))"
+echo "--------------------------------------------"
